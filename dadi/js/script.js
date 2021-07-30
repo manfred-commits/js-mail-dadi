6a3f1ad470c4d58c0ba541ff8d94e7a3332d90af
manfred-commits <manfredipiraino@gmail.com>
2021-07-30 14:57:25 +0200

    aggiunta condizioni per i dadi, fine esercizio

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -8,3 +8,11 @@ var numeroGiocatore = Math.floor(Math.random() * 6);
 var numeroComputer = Math.floor(Math.random() * 6);
 
 //2) Stabilire il vincitore, in base a chi fa il punteggio più alto.
+if(numeroGiocatore>numeroComputer){
+    alert("Il numero del giocatore è " + numeroGiocatore + ", numero superiore al numero del computer: "+ numeroComputer + "\nIl giocatore ha vinto!!!");
+}else if(numeroGiocatore<numeroComputer){
+    alert("Il numero del computer è " + numeroComputer + ", numero superiore al numero del Giocatore: "+ numeroGiocatore + "\nIl computer ha vinto!!!");
+
+}else{
+    alert("Il giocatore ed il computer hanno lo stesso numero" + "\n'numero computer: "+numeroComputer +"' 'numero giocatore: "+numeroGiocatore+"'\nVINCE IL CASO!!!");
+}
