f874b4ba0e3cfbc8d5544b1b3525a850875d20cd
manfred-commits <manfredipiraino@gmail.com>
2021-07-30 14:36:17 +0200

    aggiunto ciclo for di base

---
+++ Mail/js/script.js
@@ -0,0 +1,14 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// 1)Chiedi all’utente la sua email
+var emailUtente = prompt("Inserisci l'email che si desidera verificare: ");
+// 2)controlla che sia nella lista di chi può accedere
+// 2.1)questo passaggio richiede in primo luoga che ci sia un array contenente una lista di email
+var listaDiEmail= ["fabriziomadrigali@gmail.com","alessandromastrobbattista@gmail.com","fabriziolodigiani@gmail.com","gianpippogianpappo@gmail.com"];
+// 2.2) in secondo luogo richiede che vi sia un ciclo for con una condizione che verifichi la presenza dell'email inserita dall'utente nella nostra lista di email
+for(var i=0; i<=listaDiEmail.length; i++){
+    
+}
